ed32e4c188ba0dacc34cd501d6d9b7b782c9c204
StrawbErika <erika.nepomuceno392@gmail.com>
2020-07-31 21:38:04 +0800

    Padding and scrolling issue fixed, adjusting Text Description Container

--- src/Pages/WorkPage/index.js
+++ src/Pages/WorkPage/index.js
@@ -5,19 +5,39 @@ import project1 from "./../../Assets/Images/minkSquare.jpg";
 
 export function WorkPage() {
   return (
+    // id="works"
     <div className={styles.work}>
+      {/* <div id="works" className={styles.work}> */}
       <Project
         Title={"SINING"}
         Description={"stupid SP lmao"}
         Count={"01"}
         Percent={"25%"}
         Picture={project1}
+        // PageId={"works"}
       />
       <Project
         Title={"not"}
         Description={"stupid not lmao"}
         Count={"02"}
         Percent={"50%"}
+        Picture={project1}
+        // PageId={"works"}
+      />
+      <Project
+        Title={"not"}
+        Description={"stupid not lmao"}
+        Count={"03"}
+        Percent={"75%"}
+        Picture={project1}
+        // PageId={"works"}
+      />
+
+      <Project
+        Title={"not"}
+        Description={"stupid not lmao"}
+        Count={"04"}
+        Percent={"100%"}
         Picture={project1}
         PageId={"works"}
       />
